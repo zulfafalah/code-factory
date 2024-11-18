91a2425351a0e3c8eb1a8c0a8a44ac44b32ac881
masterwarefun <masterwarefun@gmail.com>
2024-11-18 03:15:25 +0000

    update idx project

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -19,6 +19,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "zhuangtongfa.Material-theme"
     ];
 
     # Enable previews
