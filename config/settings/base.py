9523fb0958599e9250f65bfc8c6d986f65d3677f
masterwarefun <masterwarefun@gmail.com>
2024-11-20 01:02:21 +0000

    add master models

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "palpal.users",
+    "master"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- master/admin.py
+++ master/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Item, ItemGroup, Tag, Category, Cookie
+
+admin.site.register(Item)
+admin.site.register(ItemGroup)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Cookie)
