010510b83853407042687f08e821a2aaf4537fb1
masterwarefun <masterwarefun@gmail.com>
2025-04-16 06:57:33 +0000

    update master model

--- master/admin.py
+++ master/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Category
-from .models import Cookie
 from .models import Item
 from .models import ItemGroup
 from .models import Tag
@@ -9,5 +7,3 @@ from .models import Tag
 admin.site.register(Item)
 admin.site.register(ItemGroup)
 admin.site.register(Tag)
-admin.site.register(Category)
-admin.site.register(Cookie)
