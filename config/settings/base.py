bd3036a3b5f294fd46c2f47140090d1b1db34376
zulfafalah <zulfafalah@samb.co.id>
2025-06-02 15:54:37 +0700

    remove MFA support from django-allauth and update requirements

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,7 +75,6 @@ THIRD_PARTY_APPS = [
     "crispy_bootstrap5",
     "allauth",
     "allauth.account",
-    "allauth.mfa",
     "allauth.socialaccount",
     "django_celery_beat",
     "rest_framework",
