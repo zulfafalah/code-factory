15e83fa21ddb6e043de99b622aeb604ac1d3a287
zulfafalah <zulfafalah@samb.co.id>
2025-06-05 14:54:55 +0700

    Set DJANGO_SETTINGS_MODULE in Celery startup scripts and update celery_app.py for environment-specific settings

--- config/celery_app.py
+++ config/celery_app.py
@@ -3,7 +3,11 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.local":
+    from config.settings.local import *  # noqa: F403, F401
+if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
+    from config.settings.production import *  # noqa: F403, F401
 
 app = Celery("palpal")
 
