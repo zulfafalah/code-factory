a953f3393e4255afafa39a6de3ddb854f302fb32
zulfa falah <zulfafalah@gmail.com>
2025-06-20 07:49:36 +0700

    Add Cookie model with JSON field and validation for cookie data

--- master/admin.py
+++ master/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Item
 from .models import ItemGroup
 from .models import Tag
+from .models import Cookie
 
 admin.site.register(Item)
 admin.site.register(ItemGroup)
 admin.site.register(Tag)
+admin.site.register(Cookie)
