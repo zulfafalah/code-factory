455def7bcdd24a0b06c1c33ec758b7c6b72a6b9c
zulfafalah <zulfafalah@samb.co.id>
2025-12-17 12:47:33 +0700

    feat: Introduce `kokorean` Django app and remove obsolete Nginx bare-metal setup and demo files.

--- config/settings/base.py
+++ config/settings/base.py
@@ -88,6 +88,7 @@ LOCAL_APPS = [
     "master",
     "sales",
     "yttoolkit",
+    "kokorean",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
