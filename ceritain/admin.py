ea37ab8be4daf927ad51ad4cb3aee4260b1a9b57
zulfa falah <zulfafalah@gmail.com>
2026-02-16 23:27:55 +0700

    refactor: Convert `request.user` to string when assigning to `created_by` and `updated_by` fields in the admin.

--- ceritain/admin.py
+++ ceritain/admin.py
@@ -16,8 +16,8 @@ class StoryNarrationAdmin(ModelAdmin):
 
     def save_model(self, request, obj, form, change):   
         if not change:  # Creating a new object
-            obj.created_by = request.user
-        obj.updated_by = request.user
+            obj.created_by = str(request.user)
+        obj.updated_by = str(request.user)
         super().save_model(request, obj, form, change)
 
     fieldsets = (
